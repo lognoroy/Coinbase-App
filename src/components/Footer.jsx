f84a824099cc5599cf49b00d8ea48b553e6f1f18
lognoroy <lognoroy2000@gmail.com>
2023-01-13 15:35:17 +0530

    2nd commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-const avatarSrc = "https://avatars.githubusercontent.com/u/25058652";
-
 const Footer = () => {
   return (
     <Box
@@ -26,8 +24,7 @@ const Footer = () => {
         </VStack>
 
         <VStack>
-          <Avatar boxSize={"28"} mt={["4", "0"]} src={avatarSrc} />
-          <Text>Our Founder</Text>
+          <Text>Mouri Roy</Text>
         </VStack>
       </Stack>
     </Box>
